59d9037a5979e436892155a9d1a1011c3166f4ce
waynegoh97 <waynegoh97@gmail.com>
2023-12-12 22:47:03 +0800

    Add a few new features
    Include a new function to find best server based on HTTP request and reply time
    Include 3 variations to find download speed based on user's input

--- internet_connection_check.py
+++ internet_connection_check.py
@@ -41,10 +41,49 @@ def run_speed_test(urls):
 		all_speed.append(speed)
 		time.sleep(2)
 	print(f'Your download speed is {round(sum(all_speed)/len(all_speed))} Mbps')
-	
-urls = [ 'http://link.testfile.org/150MB',
-				 'http://www.speedtest.com.sg/test_random_100mb.zip?p=f08a4ffb20b7ca9a6ae12278aaed7fc617023819786578499a39e8a',
-				 'https://link.testfile.org/PDF200MB']
+
+def find_best_server(server):
+	response_time = []
+	for db in server:
+		response = requests.get(db)
+		response_time.append(response.elapsed.total_seconds())
+	return server[response_time.index(min(response_time))]
+
+
+database_server = ['http://link.testfile.org', 'http://www.speedtest.com.sg', 'http://ipv4.download.thinkbroadband.com']
+database_url = {database_server[0]:['https://link.testfile.org/150MB', 'https://link.testfile.org/PDF200MB','https://link.testfile.org/70MB', 'https://link.testfile.org/PDF50MB'],
+								database_server[1]:['http://www.speedtest.com.sg/test_random_100mb.zip?p=f08a4ffb20b7ca9a6ae12278aaed7fc617023819786578499a39e8a', 'http://www.speedtest.com.sg/test_random_100mb.zip',
+								'http://www.speedtest.com.sg/test_random_10mb.zip', 'http://www.speedtest.com.sg/test_random_10mb.zip?p=db5398b807ede45b8e05158294ce423a1702389203657865d3dda48'],
+								database_server[2]:['http://ipv4.download.thinkbroadband.com/200MB.zip','http://ipv4.download.thinkbroadband.com/100MB.zip', 'http://ipv4.download.thinkbroadband.com/50MB.zip', 
+									 'http://ipv4.download.thinkbroadband.com/20MB.zip']}
+
+
+user_select = int(input(f'''
+Please select a number:
+										1. Single file from a server
+										2. Multiple files from same server
+										3. Multiple files from different servers
+										'''))
 
 internet_connection_test()
-run_speed_test(urls)
+### simple speed test ###
+if user_select == 1:
+	single_test = []
+	single_test.append(database_url[find_best_server(database_server)][0])
+	run_speed_test(single_test)
+
+### multiple speed test single server ###
+elif user_select == 2:
+	run_speed_test(database_url[find_best_server(database_server)])
+
+### multiple speed test multiple server ###
+elif user_select == 3:
+	url_list = []
+	for db in database_server:
+		url_list.append(database_url[db][0])
+	run_speed_test(url_list)
+
+else:
+	print("Incorrect input... Please try again")
+	
+

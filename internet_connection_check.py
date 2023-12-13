9c71e6799c142782161df6a53f767d7cc94bdeae
waynegoh97 <waynegoh97@gmail.com>
2023-12-13 21:14:44 +0800

    Modify ping for different system to use for troubleshooting

--- internet_connection_check.py
+++ internet_connection_check.py
@@ -1,36 +1,30 @@
 import requests
 import time
+import platform
 from requests.exceptions import ConnectionError
 
 # Cheng Yi's recommendation
 import os
-def internet_connection_test(url:str):
-  print(f'Attempting to connect to {url} to determine internet connection status.')
-  
-  try:
-    # ping uses ICMP protocol
-  	# pinging with 5 packets via -c flag with 10 second timeout
-   response = os.popen(f"ping -c 5 -t 10 {url}")
-   for line in response.readlines():
-      print(line)
-  except:
-    print(f"Failed with unparsed reason.")
+from re import findall
+def ping_test(url:str):
+	print(f'Attempting to ping to {url} ')
+	try:
+	# ping uses ICMP protocol
+	# pinging with 5 packets via -c flag with 10 second timeout
+		if platform.system()=="Linux":
+			response = os.popen(f'ping -c 5 -w 10 {url}')
+		elif platform.system()=="Windows":
+			response = os.popen(f'ping -n 5 -w 10000 {url}')
+		elif platform.system()=="Darwin":
+			os.popen(f"ping -c 5 -t 10 {url}")
+		else:
+			print("Cannot determine OS")
+		for line in response.readlines():
+			print(line)
+
+	except:
+		print(f"Failed with unparsed reason.")
     
-  # uses HTTP protocol
-	# try:
-	# 	print(url)
-	# 	resp = requests.get(url, timeout = 10)
-	# 	resp.text
-	# 	resp.status_code
-	# 	print(f'Connection to {url} was successful.')
-	# 	return True
-	# except ConnectionError as e:
-	# 	requests.ConnectionError
-	# 	print(f'Failed to connect to {url}.')
-	# 	return False
-	# except:
-	# 	print(f'Failed with unparsed reason.')
-	# 	return False
 
 def internet_connection_test():
 	url = 'https://www.google.com/'
@@ -46,6 +40,7 @@ def internet_connection_test():
 	except ConnectionError as e:
 		requests.ConnectionError
 		print(f'Failed to connect to {url}.')
+		ping_test("google.com")
 		return False
 	except:
 		print(f'Failed with unparsed reason.')
